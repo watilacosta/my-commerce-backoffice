e15794f6d87d6663c58959286cd52e5b7c7a73e5
Watila Costa <watila.costa@redealumni.com>
2023-01-26 00:13:18 -0300

    config bulma css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "../node_modules/bulma/css/bulma.css";
+
 const app = createApp(App);
 
 app.use(createPinia());
