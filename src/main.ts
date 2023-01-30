9cc5f52b4379e195e5f1790c5c2e1940d26499e7
watila costa <watilafreire26@gmail.com>
2023-01-29 21:02:43 -0300

    login pageg

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,22 @@ import router from "./router";
 
 import "../node_modules/bulma/css/bulma.css";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faEnvelope, faLock)
+
 const app = createApp(App);
+
+/* add font awesome icon component */
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,67 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+
+const formSubmit = () => {
+  console.log(email.value);
+  console.log(password.value);
+  email.value = "";
+  password.value = "";
+};
+</script>
 
 <template>
-  <section class="hero has-background-grey-lighter is-bold is-fullheight">
+  <section class="hero is-primary is-fullheight">
     <div class="hero-body">
-      <div class="container box">
+      <div class="container">
         <div class="columns is-centered">
-          <div class="column">
-            
+          <div class="column is-5-tablet is-4-desktop is-4-widescreen">
+            <form @submit.prevent="formSubmit" class="box">
+              <div class="field">
+                <label for="email" class="label">Email</label>
+                <div class="control has-icons-left">
+                  <input
+                    v-model="email"
+                    type="email"
+                    autocomplete="autocomplete"
+                    placeholder="example.com"
+                    class="input"
+                    required
+                  />
+                  <span class="icon is-small is-left">
+                    <font-awesome-icon icon="fa-solid fa-envelope" />
+                  </span>
+                </div>
+              </div>
+              <div class="field">
+                <label for="" class="label">Password</label>
+                <div class="control has-icons-left">
+                  <input
+                    v-model="password"
+                    type="password"
+                    placeholder="*****"
+                    class="input"
+                    required
+                  />
+                  <span class="icon is-small is-left">
+                    <font-awesome-icon icon="fa-solid fa-lock" />
+                  </span>
+                </div>
+              </div>
+              <div class="field">
+                <label for="" class="checkbox">
+                  <input type="checkbox" />
+                  Remember me
+                </label>
+              </div>
+              <div class="field mt-5">
+                <button class="button is-success is-fullwidth">Entrar</button>
+              </div>
+            </form>
           </div>
         </div>
-        <div class="title">Login</div>
-        <button class="button is-primary">ENTRAR</button>
       </div>
     </div>
   </section>
