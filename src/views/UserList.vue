8cb436b1bbac2280bea64448ade472fcab8f7185
Watila Costa <watila.costa@redealumni.com>
2023-02-11 19:49:31 -0300

    better flow to users list

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -6,20 +6,39 @@ import api from "@/utils/api";
 let users = ref([]);
 
 onMounted(async () => {
-  // TODO: trocar essa chamada direta na api em um service
   const result = await api.get("/admin/users");
-  users.value = result.data.data;
+  users.value = result.data;
 });
 </script>
 
 <template>
   <PageTitle title="Lista de Usuários" />
-  <div v-for="user in users" :key="user.attributes.id">
-    <p>
-      <span>
-        <strong>{{ user.id }}</strong>
-      </span>
-      - {{ user.attributes.firstname }}
-    </p>
+  <div class="table-container">
+    <table
+      class="table is-bordered is-narrow is-hoverable is-striped is-fullwidth my-5"
+    >
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Email</th>
+          <th class="has-text-centered">Perfil</th>
+          <th class="is-hidden-mobile has-text-centered">Status</th>
+          <th class="has-text-centered">Opções</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          :class="user.profile === 'admin' ? 'has-background-info-dark has-text-white' : ''"
+          v-for="user in users"
+          :key="user.id"
+        >
+          <td>{{ `${user.firstname} ${user.lastname}` }}</td>
+          <td>{{ user.email }}</td>
+          <td class="has-text-centered">{{ user.profile }}</td>
+          <td class="is-hidden-mobile has-text-centered">{{ user.status }}</td>
+          <td class="has-text-centered"></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
