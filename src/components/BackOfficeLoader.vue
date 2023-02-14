98125ba912064bc2f20e951ca6e970a45f0d913d
Watila Costa <watila.costa@redealumni.com>
2023-02-13 22:17:16 -0300

    added transition in table users list

--- src/components/BackOfficeLoader.vue
+++ src/components/BackOfficeLoader.vue
@@ -1,37 +1,20 @@
 <script setup>
-import { FadeLoader } from "vue3-spinner";
+import { SyncLoader } from "vue3-spinner";
 const props = defineProps({ loading: Boolean });
-
-const color = "#33CC99";
 </script>
 
 <template>
   <div id="loader">
-    <FadeLoader :loading="props.loading" :color="color" :size="300" />
+    <SyncLoader :loading="props.loading" />
   </div>
 </template>
 
 <style>
 #loader {
   position: fixed;
-  left: 50%;
+  left: 49%;
   top: 50%;
-  width: 100%;
-  height: 100%;
   z-index: 9999;
-  text-align: center;
   vertical-align: middle;
-}
-
-#loader:before {
-  content: attr(data-wordLoad);
-  position: absolute;
-  top: calc(50% + 150px);
-  left: calc(50% - 90px);
-  width: 180px;
-  display: table-cell;
-  text-align: center;
-  vertical-align: middle;
-  font-size: 1.5rem;
 }
 </style>
--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -6,7 +6,7 @@ import BackOfficeLoader from "@/components/BackOfficeLoader.vue";
 
 let users = ref([]);
 let loading = ref(true);
-let hiddenTable = ref("is-hidden");
+let showTable = ref(false);
 
 onMounted(async () => {
   const result = await api({
@@ -17,7 +17,7 @@ onMounted(async () => {
 
   users.value = result.data;
   loading.value = false;
-  hiddenTable.value = "";
+  showTable.value = true;
 });
 </script>
 
@@ -25,28 +25,42 @@ onMounted(async () => {
   <PageTitle title="Lista de Usuários" class="has-text-centered my-4" />
   <BackOfficeLoader :loading="loading" />
   <div class="table-container">
-    <table
-      class="table is-bordered is-narrow is-hoverable is-striped is-fullwidth my-3"
-      :class="hiddenTable"
-    >
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Email</th>
-          <th class="has-text-centered">Perfil</th>
-          <th class="is-hidden-mobile has-text-centered">Status</th>
-          <th class="has-text-centered">Opções</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in users" :key="user.id">
-          <td>{{ `${user.firstname} ${user.lastname}` }}</td>
-          <td>{{ user.email }}</td>
-          <td class="has-text-centered">{{ user.profile }}</td>
-          <td class="is-hidden-mobile has-text-centered">{{ user.status }}</td>
-          <td class="has-text-centered"></td>
-        </tr>
-      </tbody>
-    </table>
+    <Transition>
+      <table
+        class="table is-bordered is-narrow is-hoverable is-striped is-fullwidth my-3"
+        v-if="showTable"
+      >
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th class="has-text-centered">Perfil</th>
+            <th class="is-hidden-mobile has-text-centered">Status</th>
+            <th class="has-text-centered">Opções</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ `${user.firstname} ${user.lastname}` }}</td>
+            <td>{{ user.email }}</td>
+            <td class="has-text-centered">{{ user.profile }}</td>
+            <td class="is-hidden-mobile has-text-centered">{{ user.status }}</td>
+            <td class="has-text-centered"></td>
+          </tr>
+        </tbody>
+      </table>
+    </Transition>
   </div>
 </template>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
