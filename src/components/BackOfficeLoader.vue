929c49f179987812ef791093d0cab9f2208a56cd
Watila Costa <watila.costa@redealumni.com>
2023-02-13 21:43:02 -0300

    add loader in list users

---
+++ src/components/BackOfficeLoader.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { FadeLoader } from "vue3-spinner";
+const props = defineProps({ loading: Boolean });
+
+const color = "#33CC99";
+</script>
+
+<template>
+  <div id="loader">
+    <FadeLoader :loading="props.loading" :color="color" :size="300" />
+  </div>
+</template>
+
+<style>
+#loader {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  width: 100%;
+  height: 100%;
+  z-index: 9999;
+  text-align: center;
+  vertical-align: middle;
+}
+
+#loader:before {
+  content: attr(data-wordLoad);
+  position: absolute;
+  top: calc(50% + 150px);
+  left: calc(50% - 90px);
+  width: 180px;
+  display: table-cell;
+  text-align: center;
+  vertical-align: middle;
+  font-size: 1.5rem;
+}
+</style>
--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -2,20 +2,32 @@
 import PageTitle from "@/components/PageTitle.vue";
 import { onMounted, ref } from "vue";
 import api from "@/utils/api";
+import BackOfficeLoader from "@/components/BackOfficeLoader.vue";
 
 let users = ref([]);
+let loading = ref(true);
+let hiddenTable = ref("is-hidden");
 
 onMounted(async () => {
-  const result = await api.get("/admin/users");
+  const result = await api({
+    method: "get",
+    url: "/admin/users",
+    headers: { Authorization: sessionStorage.getItem("token") },
+  });
+
   users.value = result.data;
+  loading.value = false;
+  hiddenTable.value = "";
 });
 </script>
 
 <template>
-  <PageTitle title="Lista de Usuários" />
+  <PageTitle title="Lista de Usuários" class="has-text-centered my-4" />
+  <BackOfficeLoader :loading="loading" />
   <div class="table-container">
     <table
-      class="table is-bordered is-narrow is-hoverable is-striped is-fullwidth my-5"
+      class="table is-bordered is-narrow is-hoverable is-striped is-fullwidth my-3"
+      :class="hiddenTable"
     >
       <thead>
         <tr>
@@ -27,11 +39,7 @@ onMounted(async () => {
         </tr>
       </thead>
       <tbody>
-        <tr
-          :class="user.profile === 'admin' ? 'has-background-info-dark has-text-white' : ''"
-          v-for="user in users"
-          :key="user.id"
-        >
+        <tr v-for="user in users" :key="user.id">
           <td>{{ `${user.firstname} ${user.lastname}` }}</td>
           <td>{{ user.email }}</td>
           <td class="has-text-centered">{{ user.profile }}</td>
